9e2a1bb57944d456f76474d6b51865a9bf713381
Khang <khang06@users.noreply.github.com>
2026-02-21 14:19:55 -0500

    Prevent UiaDisconnectProvider crash on old Wine versions

--- src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.UiaDisconnectProvider.cs
+++ src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.UiaDisconnectProvider.cs
@@ -10,18 +10,25 @@ internal static partial class PInvoke
     /// <inheritdoc cref="UiaDisconnectProvider(IRawElementProviderSimple*)"/>
     public static unsafe void UiaDisconnectProvider(IRawElementProviderSimple.Interface? provider, bool skipOSCheck = false)
     {
-        if (provider is not null && (skipOSCheck || OsVersion.IsWindows8OrGreater()))
+        // PATCH: Prevent UiaDisconnectProvider crash on old Wine versions
+        try
         {
-            using var providerScope = ComHelpers.GetComScope<IRawElementProviderSimple>(provider);
-#pragma warning disable CA1416 // call site is reachable on 'windows' 6.1 and later
-            HRESULT result = UiaDisconnectProvider(providerScope);
-#pragma warning restore
-            if (result.Failed)
+            if (provider is not null && (skipOSCheck || OsVersion.IsWindows8OrGreater()))
             {
-                Debug.WriteLine($"UiaDisconnectProvider failed with {result}");
-            }
+                using var providerScope = ComHelpers.GetComScope<IRawElementProviderSimple>(provider);
+#pragma warning disable CA1416 // call site is reachable on 'windows' 6.1 and later
+                HRESULT result = UiaDisconnectProvider(providerScope);
+#pragma warning restore
+                if (result.Failed)
+                {
+                    Debug.WriteLine($"UiaDisconnectProvider failed with {result}");
+                }
 
-            Debug.Assert(result == HRESULT.S_OK || result == HRESULT.E_INVALIDARG, $"UiaDisconnectProvider failed with {result}");
+                Debug.Assert(result == HRESULT.S_OK || result == HRESULT.E_INVALIDARG, $"UiaDisconnectProvider failed with {result}");
+            }
+        }
+        catch (EntryPointNotFoundException)
+        {
         }
     }
 }
